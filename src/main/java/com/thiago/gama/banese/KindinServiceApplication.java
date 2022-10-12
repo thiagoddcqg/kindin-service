0a1c69c12fc27933b18b23c159405c11a0f029f5
Thiago Gama <thiagoddcqg@gmail.com>
2022-10-12 19:03:03 -0300

    Commit inicial

---
+++ src/main/java/com/thiago/gama/banese/KindinServiceApplication.java
@@ -0,0 +1,13 @@
+package com.thiago.gama.banese;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KindinServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KindinServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thiago/gama/banese/KindinServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thiago.gama.banese;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KindinServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
